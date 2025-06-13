38c196e22e92095f59a99207ca6ae7e975a7baca
Kshitij-Maurya-005 <kshitijmaurya005@gmail.com>
2025-06-13 22:47:04 +0530

    deployment ready

--- server.mjs
+++ server.mjs
@@ -31,6 +31,7 @@ const server = http.createServer(async (req, res) => {
     });
   }
 });
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
